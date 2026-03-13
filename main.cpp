a9b9b8147b485f77e545ff87d45dde495affb45e
Karyna Rudenko <4rudenko@stud26-02>
2026-03-13 12:45:14 +0100

    modify print

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int subtract(int a, int b) {
   return a - b; 
+}
 int add(int a, int b) { 
    return a + b; 
 } 
 
 int main() {
-  cout << "Witaj w naszym programie!" << endl;
+  cout << "Witaj w naszym programie do dzielenia!" << endl;
 
   cout << "5 - 3 = " << subtract(5, 3) << endl;
   cout << "2 + 3 = " << add(2, 3) << endl;
