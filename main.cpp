ce491d8197e1e032ddcdcf7c8937bee90226c6d1
Viktor Zasimovych <4zasimovych@stud26-01>
2026-03-13 12:43:50 +0100

    A

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int add(int a, int b) {
 } 
 
 int main() {
-  cout << "Witaj w naszym programie!" << endl;
+  cout << "Witaj w naszym programie do mnożenia!" << endl;!" << endl;
 
   cout << "2 * 3 = " << multiply(2, 3) << endl;
 
